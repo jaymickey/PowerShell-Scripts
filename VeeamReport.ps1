0306a715361baed5a100db4ef6d24949db9544c0
Josh Michielsen <josh.michielsen@kitickey.com>
2016-11-16 22:52:00 +0800

    Added some comments to VeeamReport.ps1

--- VeeamReport.ps1
+++ VeeamReport.ps1
@@ -1,12 +1,16 @@
-﻿Add-PSSnapin Veeam*
+﻿# A basic "all-in-one" daily Veeam report with colour coded statuses
 
+Add-PSSnapin Veeam*
+
+# Get all backup jobs
 $Jobs = Get-VBRJob
 $ResultsData = @()
 
 foreach ($Job in $Jobs) { 
 
     $LastSession = $Job.FindLastSession()
-    
+
+    # Get desired properties for each job    
     $JobName = $Job.Name
     $LastResult = $Job.GetLastResult()
     $Type = $Job.JobType
@@ -15,6 +19,7 @@ foreach ($Job in $Jobs) {
     $Duration = '{0:00}:{1:00}:{2:00}' -f (New-TimeSpan -Start $StartTime -End $FinishTime | % {$_.Hours,$_.Minutes,$_.Seconds})
     $Message = $LastSession.GetDetails()
 
+    # Add the values to a custom PSobject
     $obj = New-Object PSobject
 	$obj | Add-Member -MemberType NoteProperty -name "Job Name" -value $JobName
 	$obj | Add-Member -MemberType NoteProperty -name Type -value $Type
@@ -23,12 +28,15 @@ foreach ($Job in $Jobs) {
 	$obj | Add-Member -MemberType NoteProperty -name Finish -value $FinishTime
 	$obj | Add-Member -MemberType NoteProperty -name Duration -value $Duration
 	$obj | Add-Member -MemberType NoteProperty -name Message -value $Message
+    # Add to results array
 	$ResultsData += $obj
 
 }
 
+# Sort by Job Name
 $ResultsData = $ResultsData | sort -Property "Job Name"
 
+# CSS Styles for HTML email
 $EmailHTML = @"
 <style type="text/css">
 
@@ -59,15 +67,18 @@ $EmailHTML = @"
 </style>
 "@
 
+# Convert Result data array to HTML
 $ResultsHTML = $ResultsData | ConvertTo-Html -Head $EmailHTML
 $ResultsHTML = $ResultsHTML -replace (">Success"," style='background-color:#00AA00'>Success")
 $ResultsHTML = $ResultsHTML -replace (">Failed"," style='background-color:red'>Failed")
 $ResultsHTML = $ResultsHTML -replace (">Warning"," style='background-color:orange'>Warning")
 
+# Set email properties
 $Date = Get-Date -format "dd-MM-yyy"
 $emailTo = "sysadmin@contoso.com"
 $emailSubject = "Backups Report - $Date"
 $emailFrom = "veeam@contoso.com"
 $emailServer = "mail.contoso.com"
 
+# Send email
 Send-MailMessage -To $emailTo -From $emailFrom -Subject $emailSubject -SmtpServer $emailServer -Body "$ResultsHTML" -BodyAsHtml
